99cd7f5a1472fcbb1486440273bb07ae4c4ae994
Alex Bulanov <alexander@bulanov.com>
2023-01-25 14:27:10 -0500

    fixing dotenv integration

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,11 +1,11 @@
 import * as firebase from 'firebase';
 import '@firebase/auth';
 import '@firebase/firestore';
-import {config} from 'dotenv';
+import {REACT_APP_API_KEY, REACT_APP_AUTH_DOMAIN} from '@env';
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: "cs4261-firstprogrammingassign",
   storageBucket: "cs4261-firstprogrammingassign.appspot.com",
   messagingSenderId: "460658260610",
